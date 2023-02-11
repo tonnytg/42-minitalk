19353050958ab222e38cfcfb620eae9e491ff97d
Antonio Thomacelli Gomes <tonnytg@gmail.com>
2023-02-11 15:42:27 -0300

    feat: add loop for server

--- client.c
+++ client.c
@@ -1,35 +1,13 @@
 #include <stdio.h>
-#include <unistd.h>
-#include <sys/types.h>
-#include <signal.h>
+#include <stdlib.h>
 
-#define MESSAGE_LENGTH 100
-
-void send_message(char* message) {
-	// aqui você pode enviar a mensagem codificada para o servidor
-	// através de uma conexão de rede, serial, etc
-	// e enviar o sinal SIGUSR1 para o processo pai no servidor
-	// para indicar que a mensagem está pronta para ser lida
-	kill(server_pid, SIGUSR1);
-}
-
-int main() {
-	char message[MESSAGE_LENGTH];
-
-	while (1) {
-		printf("Digite a mensagem a ser enviada: ");
-		scanf("%s", message);
-
-		// codifica a mensagem em bitwise
-		for (int i = 0; i < MESSAGE_LENGTH; i++) {
-			message[i] = message[i] ^ 0xFF;
-		}
-
-		// envia a mensagem codificada para o servidor
-		send_message(message);
+int main(int argc, char **argv)
+{
+	if (argc < 2)
+	{
+		printf("Usage: <PID> Message");
+		return 1;
 	}
 
-	return 0;
+	printf("Sending message to PID: %s\nMSG:%s\n", argv[1], argv[2]);
 }
-
-
--- server.c
+++ server.c
@@ -1,66 +1,20 @@
-#include <stdio.h>
 #include <unistd.h>
-#include <sys/types.h>
+#include <stdio.h>
 #include <signal.h>
 
-#define MESSAGE_LENGTH 100
+int main(int argc, char **argv)
+{
+	int pid;
+	int count;
 
+	pid = getpid();
+	printf("pid: %d\n", pid);
 
-void read_message() {
-	char message[MESSAGE_LENGTH];
-
-	// lê a mensagem codificada do pipe
-	read(pipefd[0], message, MESSAGE_LENGTH);
-
-	// decodifica a mensagem
-	for (int i = 0; i < MESSAGE_LENGTH; i++) {
-		message[i] = message[i] ^ 0xFF;
+	count = 0;
+	while(count < 1000)
+	{
+		sleep(1);
+		count += 1;
 	}
-
-	printf("Mensagem recebida: %s\n", message);
-}
-
-
-int main() {
-	pid_t pid;
-	int pipefd[2];
-	char message[MESSAGE_LENGTH];
-
-	// cria um pipe para comunicação entre processos
-	pipe(pipefd);
-
-	// cria o processo filho
-	pid = fork();
-
-	if (pid == 0) {
-		// processo filho
-		close(pipefd[0]); // fecha o lado de leitura do pipe
-
-		while (1) {
-			printf("Digite a mensagem a ser enviada: ");
-			scanf("%s", message);
-
-			// codifica a mensagem em bitwise
-			for (int i = 0; i < MESSAGE_LENGTH; i++) {
-				message[i] = message[i] ^ 0xFF;
-			}
-
-			// escreve a mensagem codificada no pipe
-			write(pipefd[1], message, MESSAGE_LENGTH);
-
-			// envia o sinal SIGUSR1 para o processo pai
-			kill(getppid(), SIGUSR1);
-		}
-	} else {
-		// processo pai
-		close(pipefd[1]); // fecha o lado de escrita do pipe
-
-		while (1) {
-			// aguarda pelo sinal SIGUSR1
-			signal(SIGUSR1, read_message);
-			pause();
-		}
-	}
-
 	return 0;
 }
