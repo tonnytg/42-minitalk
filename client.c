b7cf42e2853d1c781bb7792fdf961f23bdc5f835
Antonio Thomacelli Gomes <tonnytg@gmail.com>
2023-02-12 10:36:08 -0300

    feat: add new client format

--- client.c
+++ client.c
@@ -1,13 +1,27 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <signal.h>
 
-int main(int argc, char **argv)
+int	main(int argc, char **argv)
 {
+	int p;
+	int err;
+	int msg;
+
 	if (argc < 2)
 	{
 		printf("Usage: <PID> Message");
 		return 1;
 	}
-
-	printf("Sending message to PID: %s\nMSG:%s\n", argv[1], argv[2]);
+	p = atoi(argv[1]);
+	msg = atoi(argv[2]);
+	printf("Sending message to PID: %d\nMSG:%d\n", p, msg);
+	err = kill(p, msg);
+	sigaction(SIGUSR1, NULL, NULL);
+	if (err == -1)
+	{
+		printf("Error sending message\n");
+		return 1;
+	}
+	printf("kill sent\n");
 }
