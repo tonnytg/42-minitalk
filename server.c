da67e07c8fd13510ecbb228216afc6b418ec3cc5
Antonio Thomacelli Gomes <tonnytg@gmail.com>
2023-02-12 10:35:55 -0300

    feat: add new format for server

--- server.c
+++ server.c
@@ -1,20 +1,25 @@
-#include <unistd.h>
+#include <stdlib.h>
 #include <stdio.h>
+#include <unistd.h>
 #include <signal.h>
 
-int main(int argc, char **argv)
+struct Config {
+	int delay;
+};
+
+int	main(int argc, char **argv)
 {
 	int pid;
-	int count;
+	struct Config config;
 
 	pid = getpid();
-	printf("pid: %d\n", pid);
-
-	count = 0;
-	while(count < 1000)
+	config.delay = argv[1] ? atoi(argv[1]) : 0;
+	printf("Delay: %d\n", config.delay);
+	while (config.delay > 0)
 	{
+		printf("Waiting for %d seconds - pid: %d\n", config.delay, pid);
 		sleep(1);
-		count += 1;
+		config.delay--;
 	}
-	return 0;
+	printf("Done waiting\n");
 }
