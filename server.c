783fad9f2c02bbe4b8c2b72ee84c63f9ff12cdd8
Antonio Thomacelli Gomes <tonnytg@gmail.com>
2023-02-13 21:06:06 -0300

    feat: test with mask

--- server.c
+++ server.c
@@ -7,6 +7,23 @@ struct Config {
 	int delay;
 };
 
+void signal_handler(int sig)
+{
+	// decode signal
+	sigset_t set;
+	struct sigaction act;
+
+	set = 1 >> (sig + 1);
+	sigemptyset(&set);
+	sigaddset(&set, sig);
+	act.sa_mask = set;
+	act.sa_flags = 0;
+	act.__sigaction_u.__sa_handler = NULL;
+	sigaction(sig, &act, NULL);
+
+	printf("Received signal: %d\n", sig);
+}
+
 int	main(int argc, char **argv)
 {
 	int pid;
@@ -18,6 +35,8 @@ int	main(int argc, char **argv)
 	while (config.delay > 0)
 	{
 		printf("Waiting for %d seconds - pid: %d\n", config.delay, pid);
+		signal(SIGUSR1, signal_handler);
+		signal(SIGUSR2, signal_handler);
 		sleep(1);
 		config.delay--;
 	}
