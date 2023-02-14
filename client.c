21b9630b890bfed38ada717bd31216f4794f50c2
Antonio Thomacelli Gomes <tonnytg@gmail.com>
2023-02-13 21:05:25 -0300

    feat: add new test with sigemptyset

--- client.c
+++ client.c
@@ -7,21 +7,30 @@ int	main(int argc, char **argv)
 	int p;
 	int err;
 	int msg;
+	sigset_t set;
+	struct sigaction act;
 
-	if (argc < 2)
+	if (argc <= 2)
 	{
-		printf("Usage: <PID> Message");
+		printf("Usage: <PID> Message\n");
 		return 1;
 	}
 	p = atoi(argv[1]);
 	msg = atoi(argv[2]);
-	printf("Sending message to PID: %d\nMSG:%d\n", p, msg);
-	err = kill(p, msg);
-	sigaction(SIGUSR1, NULL, NULL);
-	if (err == -1)
+
+	set = 1 << (msg - 1);
+	sigemptyset(&set);
+	sigaddset(&set, msg);
+	act.sa_mask = set;
+	act.sa_flags = 0;
+	act.__sigaction_u.__sa_handler = NULL;
+	err = sigaction(msg, &act, NULL);
+	if (err)
 	{
-		printf("Error sending message\n");
+		printf("Error: %d\n", err);
 		return 1;
 	}
-	printf("kill sent\n");
+
+	printf("Sending message to PID: %d\nMSG:%d\n", p, msg);
+	return 0;
 }
