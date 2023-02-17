5e1c1741bff295e74704468d722aad29b58ff666
Antonio Thomacelli Gomes <tonnytg@gmail.com>
2023-02-17 17:18:40 -0300

    feat: add new header

---
+++ minitalk.h
@@ -0,0 +1,26 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   minitalk.h                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: antthoma <antthoma@student.42sp.org.br>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/01/02 23:31:58 by antthoma          #+#    #+#             */
+/*   Updated: 2023/02/17 15:37:06 by antthoma         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef MINITALK_H
+# define MINITALK_H
+
+# include <signal.h>
+# include <unistd.h>
+# include "libft.h"
+
+typedef struct t_bits
+{
+	char				bits[8];
+	int					n;
+}t_bits;
+
+#endif
